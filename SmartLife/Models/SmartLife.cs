f4162b529f2f36859d0c45635348674ae004034e
pengchengli <n01715807@humber.ca>
2025-06-29 18:24:22 -0400

    Fix: ignore .vs and obj folders

---
+++ SmartLife/Models/SmartLife.cs
@@ -0,0 +1,12 @@
+public class SmartLifeClass
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required int Age { get; set; }
+    public required string Occupation { get; set; }
+    public required decimal Money { get; set; }
+    public required string Mood { get; set; }
+    public required string Personality { get; set; }
+    public required int Familybond { get; set; }
+    public DateTime LastUpdated { get; set; }
+}
